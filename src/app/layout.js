8d975e137a35c2bbd23d7b984d3a701fabadb129
AnnaFutryk <anna.futryk@gmail.com>
2025-03-04 16:47:39 +0200

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -68,7 +68,7 @@ export default function RootLayout({ children }) {
             src="https://www.googletagmanager.com/ns.html?id=GTM-N9HGZTT4"
             height="0"
             width="0"
-            style="display:none;visibility:hidden"
+            style={{ display: "none", visibility: "hidden" }}
           ></iframe>
         </noscript>
 
